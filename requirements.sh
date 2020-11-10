b1d499e3e82efe94dfcf919c2abab67a934cbf8f
choeoe <835095255@qq.com>
2020-11-10 10:22:30 +0800

    init

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+rvm install 2.6
+rvm use 2.6 --default
+sudo apt-get update 
+sudo apt-get install -y nodejs
+sudo apt install npm
+sudo npm cache clean
+sudo npm install -g n
+sudo    n   stable
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt install --no-install-recommends yarn
+gem install bundler
