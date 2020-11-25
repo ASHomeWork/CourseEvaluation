d805fdfec39e12c6f4593881f31ec739d9c71519
choeoe <835095255@qq.com>
2020-11-25 20:31:36 +0800

    Update user password

--- CourseEvaluation/db/migrate/20201124120230_create_users.rb
+++ CourseEvaluation/db/migrate/20201124120230_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :user_name, null: false
-      t.string :password, limit: 128, null: false
+      t.string :password_digest, null: false
       t.string :email, null: false
       t.integer :power, null: false
       t.references :gender, null: false, foreign_key: true
