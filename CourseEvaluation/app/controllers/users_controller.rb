5e639960c6119c8477e5ad7a45c3f9a40f980fbc
cxc <3162636675@qq.com>
2021-01-09 16:18:20 +0800

    修改申请表单错误提示

--- CourseEvaluation/app/controllers/users_controller.rb
+++ CourseEvaluation/app/controllers/users_controller.rb
@@ -28,14 +28,41 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.power = 1
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to root_url, notice: '创建用户成功.' }
-        format.json { render :show, status: :created, location: @user }
+    if @user.save
+      flash.now[:success] = "创建用户成功"
+      redirect_to '/'
+    else
+      if @user.user_name == ""
+        flash.now[:name] = "用户名非空"
+        render :action=>'new'
+      elsif User.find_by(user_name:@user.user_name)
+        flash.now[:rename] = "用户名已存在"
+        render :action=>'new'
+      elsif @user.user_name.length < 2
+        flash.now[:short] = "用户名太短"
+        render :action=>'new'
+      elsif @user.user_name.length > 50
+        flash.now[:long] = "用户名太长"
+        render :action=>'new' 
+      elsif @user.password == nil
+        flash.now[:password] = "请输入密码"
+        render :action=>'new' 
+      elsif @user.password.length < 6
+        flash.now[:password1] = "输入密码长度过短"
+        render :action=>'new' 
+      elsif @user.password_confirmation != @user.password
+        flash.now[:diff] = "请两次输入相同的密码"
+        render :action=>'new' 
+      elsif User.find_by(email:@user.email)
+        flash.now[:reemail] = "此邮箱已经注册过账号，请重新输入"
+        render :action=>'new' 
+      elsif !(@user.email =~ /(.)*@mails.ucas.ac.cn/)
+        flash.now[:email] = "邮箱请以@mails.ucas.ac.cn结尾"
+        render :action=>'new'
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        flash.now[:gener] = "请选择性别"
+        render  :action=>'new'
+      end 
     end
   end
 
