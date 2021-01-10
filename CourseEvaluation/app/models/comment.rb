f467143d5758d6dd43cee016cfbf1ad5644566a0
fxy <1162717601@qq.com>
2021-01-10 17:55:42 +0800

    model_like

--- CourseEvaluation/app/models/comment.rb
+++ CourseEvaluation/app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :user
     has_many :reviews
     belongs_to :course
+    has_many :likes, through: :users,dependent: :destroy 
     
     def get_reviews
         review=Review.where(:comment => self)
